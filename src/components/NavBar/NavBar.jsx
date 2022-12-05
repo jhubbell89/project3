0652a9591b9b33f6f5ca6ff9756ee4617576ed79
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-12-04 23:14:09 -0800

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
-
+import './NavBar.css'
 export default function NavBar({ user, setUser }) {
 
   function handleLogOut() {
@@ -11,14 +11,18 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      &nbsp; | &nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp; | &nbsp;
-      <Link to="" onClick={handleLogOut}>Log Out</Link>
-    </nav>
+    <div class=''>
+      <nav>
+        <Link to='/'>Main</Link>
+        &nbsp; | &nbsp;
+        {/* <Link to="/orders">Order History</Link>
+        &nbsp; | &nbsp;
+        <Link to="/orders/new">New Order</Link>
+      &nbsp; | &nbsp; */}
+        <span>Welcome, {user.name}</span>
+        &nbsp; | &nbsp;
+        <Link to="" onClick={handleLogOut}>Log Out</Link>
+      </nav>
+    </div>
   );
 }
