641c2fb358f8c03f42bdb618e98f491036039464
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-12-07 16:28:25 -0800

    work on CRUD

--- src/pages/NewJokePage/NewJokePage.jsx
+++ src/pages/NewJokePage/NewJokePage.jsx
@@ -1,8 +1,34 @@
-export default function NewJokePage() {
-  return (
-    <>
-    <p>new joke page</p>
+import { Component } from 'react';
+
+export default class NewJokeForm extends Component {
+  state = {
+    joke: '',
+    nsfw: '',
+  }
+
+  handleSubmit = async (evt) => {
+    evt.preventDefault();
     
-    </>
-  );
+  }
+
+  handleChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+      error: ''
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Joke</label>
+          <input type='text' name='joke' value={this.state.joke} onChange={this.handleChange} />
+          <br />
+          <label>Appropriate?</label>
+          <input type='text' name='nsfw' />
+        </form>
+      </div>
+    )
+  }  
 }
