8e35ceea6c44637307c867e2e395432df1eccd78
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-12-08 09:57:54 -0800

    work on CRUD in utilities

--- routes/api/jokes.js
+++ routes/api/jokes.js
@@ -4,10 +4,10 @@ const jokesCtrl = require('../../controllers/api/jokes')
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 router.get('/', ensureLoggedIn, jokesCtrl.index)
-router.get('/', ensureLoggedIn, jokesCtrl.new )
+router.get('/new', ensureLoggedIn, jokesCtrl.new )
 router.post('/', ensureLoggedIn, jokesCtrl.create)
 router.delete('/:id', ensureLoggedIn, jokesCtrl.delete)
-router .get('/:id/edit', ensureLoggedIn, jokesCtrl.edit)
+router.get('/:id/edit', ensureLoggedIn, jokesCtrl.edit)
 router.put('/:id', ensureLoggedIn, jokesCtrl.update)
 
 module.exports = router;
--- src/utilities/jokes-api.js
+++ src/utilities/jokes-api.js
@@ -1,7 +1,26 @@
-import senRequest from './send-request';
+import sendRequest from './send-request';
 const BASE_URL = '/api/jokes';
 
 export function showJokes() {
-    return(`${BASE_URL}/jokes`);
+    return sendRequest(`${BASE_URL}`, 'GET');
 }
 
+export function newJoke() {
+    return sendRequest(`${BASE_URL}/new`, 'GET')
+}
+
+export function createJoke() {
+    return sendRequest(`${BASE_URL}`, 'POST')
+}
+
+export function deleteJoke() {
+    return sendRequest(`${BASE_URL}`, 'DELETE')
+}
+
+export function editJoke() {
+    return sendRequest(`${BASE_URL}`, 'GET')
+}
+
+export function updateJoke() {
+    return sendRequest(`${BASE_URL}`, 'PUT')
+}
